f92a0fde71b9a6591a7c5b719028f8b2c4d4b08d
linh2772001 <linhnguyenhihi2772001@gmail.com>
2023-07-14 01:00:15 +0700

    tinh Coin

---
+++ Assets/Scripts/Shop/UI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text m_Score;
+
+
+    public void textScore(string text)
+    {
+        if (m_Score)
+        {
+            m_Score.text = text;
+        }
+    }
+}
